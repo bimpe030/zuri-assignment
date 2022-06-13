4c32b051a852cb7dfb59eda73d925bd7e2acc844
bimpe030 <adelanwa.zainab05@gmail.com>
2022-06-13 14:53:56 +0100

    first commit

---
+++ javascript file/website.js
@@ -0,0 +1,9 @@
+// document.getElementById('wrapper').addEventListener('click', function() {
+//   var wrapper = this;
+
+//   wrapper.classList.add('flipped');
+
+//   setTimeout(function() {
+//     wrapper.classList.remove('flipped');
+//   }, 5000);
+// });
